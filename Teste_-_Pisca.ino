ea0738f0a9cc821150334b57d5730181c8705a53
PedroVicente10 <87193306+PedroVicente10@users.noreply.github.com>
2021-07-09 16:16:02 +0100

    Delete Teste_-_Pisca.ino

--- Teste_-_Pisca.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  pinMode(D0, OUTPUT);    
-}
-void loop(){
-  digitalWrite(D0, LOW);   
-  delay(500);                    
-  digitalWrite(D0, HIGH); 
-  delay(500);                      
-}
